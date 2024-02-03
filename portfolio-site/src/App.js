031d3fba9d11628aa45b05cf189f66c049d23905
hartleyleo <hartleyleo2001@gmail.com>
2024-02-03 14:00:58 -0500

    Updated homepage
    Added logo

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -2,6 +2,9 @@ import './Stylesheets/App.css';
 
 import React, { useState } from 'react';
 
+import instagramImage from './Media/Images/IMG_4752.png';
+import logo from './Media/Images/logo.png';
+
 import Home from './Home.js';
 import GalleryCarousel from './Components/GalleryCarousel.js';
 
@@ -22,6 +25,7 @@ function App() {
           <div className="navbar-title-container" onClick={() => setPageStateHelper(0)}>
             <div>Boreas Hartley</div>
           </div>
+          <div><img alt="logo for website" src={logo} /></div>
           <div className="navbar-button-container">
             <button className="nav-button" onClick={() => setPageStateHelper(1)}>Gallery</button>
             <button className="nav-button">Commissions</button>
@@ -30,7 +34,7 @@ function App() {
 
         <div className="pages">
           {pageState === 0 && (<Home />)}
-          {pageState === 1 && (<GalleryCarousel />)}
+          {pageState === 1 && (<GalleryCarousel image={instagramImage} />)}
         </div>
 
       </div>
