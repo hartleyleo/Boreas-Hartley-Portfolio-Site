ceebcd64a0a91e08877a9d106382a49138c660a8
hartleyleo <hartleyleo2001@gmail.com>
2024-02-20 19:14:15 -0500

    Login page work
    added username and password fields, still need to implement the useState versions

--- portfolio-site/src/Components/LoginPopup.js
+++ portfolio-site/src/Components/LoginPopup.js
@@ -1,21 +1,37 @@
 const LoginPopup = ({ onClose }) => {
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     return (
-        <div className="login-container" >
+        <div className="overlay">
+            <div className="login-container" >
 
-            <div>
-                <div>Username:</div>
-                <input></input>
+                <h1>Login</h1>
+
+                <div className="login-text">
+                    <div>Username</div>
+                    <input className="login-input"></input>
+                </div>
+
+                <div className="login-text">
+                    <div>Password</div>
+                    <input type="password" className="login-input password-field"></input>
+                </div>    
+
+                <div className="login-button-field">
+
+                    <button className="login-button">
+                        Login
+                    </button>
+                    <button className="close-popup-button" onClick={onClose}>
+                        Close
+                    </button>
+
+                </div>
+
+                
             </div>
-
-            <div>
-                <div>Password:</div>
-                <input></input>
-            </div>    
-
-            <button className="popup-button" onClick={onClose}>
-                Close
-            </button>
         </div>
     );
 };
